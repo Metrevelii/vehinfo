ff3074a00f4c851754a09c5583af7b283af54d2b
Metrevelii <72525534+Metrevelii@users.noreply.github.com>
2024-10-12 13:35:19 +0400

    generate token for registered users

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -4,14 +4,13 @@ const authController = {
     async register(req, res, next) {
 
         try {
-            
-
-
             const { email, password } = req.body;
             const user = await authService.createUser(email, password);
+            const token = await authService.genAuthToken(user);
 
-            res.status(200).send({
-                user
+            res.cookie('x-access-token', token).status(200).send({
+                user,
+                token
             })
         } catch (error) {
             console.log(error);
